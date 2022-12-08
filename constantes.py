14d67976441ff54b22b60efcb9b9c200ce8b34ab
vikmiranda <jvkmiranda@hotmail.com>
2022-12-07 21:21:11 -0300

    feature: testes consultar carga

--- constantes.py
+++ constantes.py
@@ -1,19 +1,22 @@
+# -*- coding: utf-8 -*-
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import random
 
 SITE_LINK = "https://rastreadordecarga-front.vercel.app/admin"
+SITE_RASTREIO = "https://rastreadordecarga-front.vercel.app/carga"
 servico = Service(ChromeDriverManager().install())
 navegador = webdriver.Chrome(service=servico)
 
 # gera um valor aleatorio para selecionar cidade
+cidades = {2: 'Macapá', 3: 'Belém', 4: 'São Luis', 5: 'Fortaleza', 6: 'Natal', 7: 'Recife', 8: 'Maceió', 9: 'Aracáju', 10: 'Salvador', 11: 'Vitória', 12: 'Rio de Janeiro', 13: 'Santos'}
 cidade_origem = random.randint(2, 13)
 cidade_destino = random.randint(2, 13)
 
 cidade_att = random.randint(2, 13)
 op_att = random.randint(3, 6)
-
 
 # ano = [2023,2024]
 # dia = str(random.randint(1, 30))
--- main.py
+++ main.py
@@ -8,8 +8,11 @@ from auxs import *
 def checar_dash(id_tabela, cod_rastreio):
     time.sleep(2)
     navegador.get(SITE_LINK)
+    navegador.find_element('xpath', '//*[@id="__next"]/div/button').click()
+    navegador.find_element('xpath', '//*[@id="__next"]/div/button').click()
+    time.sleep(2)
     try:
-        navegador.find_element('xpath', '//*[@id="visualizarDashboard"]').click()
+        navegador.find_element('xpath', '/html/body/div/div/div[1]/ul/li[2]/button').click()
     except(Exception):
         print(f'erro ao tentar clicar no dashboard \n{Exception}')
 
@@ -51,7 +54,14 @@ def nova_carga():
 def atualizar_carga(cod_rastreio):
     escrever_log(f"\nExecutando Atualização da carga: {cod_rastreio}\n")
     navegador.get(SITE_LINK)
-    navegador.find_element('xpath', '//*[@id="atualizarCarga"]').click()
+    navegador.find_element('xpath', '//*[@id="__next"]/div/button').click()
+    navegador.find_element('xpath', '//*[@id="__next"]/div/button').click()
+    time.sleep(2)
+    try:
+        navegador.find_element('xpath', '/html/body/div/div/div[1]/ul/li[3]/button').click()
+    except(Exception):
+        print(f'erro ao clicar em atualizar carga {Exception}')
+
     navegador.find_element('xpath', '//*[@id="formAtualizarCarga"]/input').send_keys(cod_rastreio)
     navegador.find_element('xpath', f'//*[@id="formAtualizarCarga"]/select[1]/option[2]').click()
     navegador.find_element('xpath', f'//*[@id="formAtualizarCarga"]/select[2]/option[4]').click()
@@ -61,9 +71,29 @@ def atualizar_carga(cod_rastreio):
     resultado = checar_dash("tabelaEntregada", cod_rastreio)
 
     if resultado:
-        escrever_log("\n-----------RESULTADO-----------:\nTeste Bem-Sucedido. Carga Atualizada com Sucesso")
+        escrever_log("\n-----------RESULTADO-----------:\nTeste Bem-Sucedido. Carga Atualizada com Sucesso\n")
     else:
         escrever_log("\n-----------RESULTADO-----------:\nTeste Falhou. A Função de Atualizar Carga não está funcionando corretamente")
+
+    consultar_carga(cod_rastreio)
+
+def consultar_carga(cod_rastreio):
+    escrever_log(f"\n\nExecutando CONSULTA da carga: {cod_rastreio}\n")
+
+    navegador.get(f'{SITE_RASTREIO}/{cod_rastreio}')
+    time.sleep(1)
+    informacoes_carga = navegador.find_elements('xpath', '//*[@id="__next"]/div/div/div')
+    informacoes_existentes = [cod_rastreio, '15/06/2023', cidades[cidade_origem], cidades[cidade_destino],'carga registrada', 'Reivindicar' ]
+    resultado = False
+    print(f'info da carga\n{informacoes_carga[0].text}')
+    #verificar se todas as informações cadastradas estão na pagina
+    for i in informacoes_existentes:
+        if str(i) in informacoes_carga[0].text:
+            resultado = True
+    if resultado:
+        escrever_log("\n-----------RESULTADO-----------:\nTeste Bem-Sucedido. Informações da Carga estão coerentes")
+    else:
+        escrever_log("\n-----------RESULTADO-----------:\nTeste Falhou. A Consulta da carga não está funcionando  corretamente")
 
 
 if __name__ == '__main__':
